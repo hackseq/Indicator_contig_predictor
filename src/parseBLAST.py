8839064bbd4dc0b48906e3be908a6aaeb4ee4f5b
cjustin <cjustin@bcgsc.ca>
2016-10-15 16:27:19 -0700

    parsing script for blast output stub

---
+++ src/parseBLAST.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author:     cjustin
+@contact:    cjustin@bcgsc.ca
+'''
+
+class ParseBLAST:
+    
+    def __init__(self, reads):
+        """Constructor"""
+        pass
+    
+    def parse(self):
+        """From stdin parse, and populate counts and output to stdout"""
+        pass
+
+    
+if __name__ == '__main__':
+    parser = ParseBLAST()
+    parser.run()
