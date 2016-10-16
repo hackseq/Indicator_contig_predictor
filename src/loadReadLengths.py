f6c1717dddcb181a914f948f0bd62af5ffca5e28
cjustin <cjustin@bcgsc.ca>
2016-10-16 14:22:47 -0700

    read length parser

---
+++ src/loadReadLengths.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author:     cjustin
+@contact:    cjustin@bcgsc.ca
+'''
+
+class ReadLengths:
+    
+    def __init__(self):
+        """Constructor"""
+        pass
+    
+    def load(self, fastaIndex):
+        self._lengths = {}
+        fh = open(fastaIndex)
+        for line in fh:
+            tempArray = line.split("\t")
+            readID = tempArray[0]
+            length = tempArray[1]
+            self._lengths[readID] = int(length)
+
+    
+if __name__ == '__main__':
+    
+    parser = OptionParser()
+    parser.add_option("-i", "--input", dest="input", metavar="INPUT",
+                      help="input file")
+                
+    (options, args) = parser.parse_args()
+    
+    rl = ReadLengths(options.input)
+    rl.load()
